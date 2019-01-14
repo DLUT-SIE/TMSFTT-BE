0349d6de11e4f0a41bbf206ef3c89e0ee04be0f2
David <youchen.du@gmail.com>
2019-01-14 15:48:23 +0800

    Fix find command in presubmit_tests.sh

--- scripts/presubmit_tests.sh
+++ scripts/presubmit_tests.sh
@@ -2,7 +2,7 @@
 echo "Checking if code meets PEP8 by Flake8."
 python -m flake8
 echo "Checking if code meets lint rules by pylint."
-find . -depth 1 -type d -not -path './.*' -not -path './mock_cas' -not -path './scripts/' -not -path './htmlcov' | xargs python -m pylint
+find . -maxdepth 1 -type d -not -path './.*' -not -path './mock_cas' -not -path './scripts' -not -path './htmlcov' -not -path '.' | xargs python -m pylint
 echo "Running unit tests and coverage test."
 python -m coverage run manage.py test --debug-mode
 python -m coverage report --skip-covered
