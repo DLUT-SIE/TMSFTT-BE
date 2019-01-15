4ba402f85546fbb412d3e42c7d4460258111fc0f
Yqzzxlj <yq736677@icloud.com>
2019-01-15 21:18:34 +0800

    fixed tests_views

--- training_record/tests/tests_views.py
+++ training_record/tests/tests_views.py
@@ -1,6 +1,8 @@
 '''Unit tests for training_record views.'''
+import io
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.utils.timezone import now
 from model_mommy import mommy
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -45,7 +47,8 @@ class TestRecordViewSet(APITestCase):
     def test_delete_record(self):
         '''Record should be deleted by DELETE request.'''
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
         url = reverse('record-detail', args=(record.pk,))
 
         response = self.client.delete(url)
@@ -56,7 +59,8 @@ class TestRecordViewSet(APITestCase):
     def test_get_record(self):
         '''Record should be accessed by GET request.'''
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
         url = reverse('record-detail', args=(record.pk,))
         expected_keys = {'id', 'create_time', 'update_time', 'campus_event',
                          'off_campus_event', 'user', 'status'}
@@ -71,8 +75,8 @@ class TestRecordViewSet(APITestCase):
         status0 = training_record.models.Record.STATUS_SUBMITTED
         status1 = training_record.models.Record.STATUS_FACULTY_ADMIN_REVIEWED
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event,
-                            status=status0)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event, status=status0)
         url = reverse('record-detail', args=(record.pk,))
         data = {'status': status1}
 
@@ -90,7 +94,8 @@ class TestRecordContentViewSet(APITestCase):
         url = reverse('recordcontent-list')
 
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
         content_type = RecordContent.CONTENT_TYPE_CONTENT
         content = "ABC"
         data = {'record': record.id, 'content_type': content_type,
@@ -119,8 +124,10 @@ class TestRecordContentViewSet(APITestCase):
     def test_delete_recordcontent(self):
         '''RecordContent should be deleted by DELETE request.'''
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
-        recordcontent = mommy.make(training_record.models.RecordContent, record=record)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
+        recordcontent = mommy.make(training_record.models.RecordContent,
+                                   record=record)
         url = reverse('recordcontent-detail', args=(recordcontent.pk,))
 
         response = self.client.delete(url)
@@ -132,8 +139,10 @@ class TestRecordContentViewSet(APITestCase):
     def test_get_recordcontent(self):
         '''RecordContent should be accessed by GET request.'''
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
-        recordcontent = mommy.make(training_record.models.RecordContent, record=record)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
+        recordcontent = mommy.make(training_record.models.RecordContent,
+                                   record=record)
         url = reverse('recordcontent-detail', args=(recordcontent.pk,))
         expected_keys = {'id', 'create_time', 'update_time', 'record',
                          'content_type', 'content'}
@@ -148,7 +157,8 @@ class TestRecordContentViewSet(APITestCase):
         content_type0 = RecordContent.CONTENT_TYPE_CONTENT
         content_type1 = RecordContent.CONTENT_TYPE_SUMMARY
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
         recordcontent = mommy.make(training_record.models.RecordContent,
                                    record=record, content_type=content_type0)
         url = reverse('recordcontent-detail', args=(recordcontent.pk,))
@@ -166,22 +176,19 @@ class TestRecordAttachmentViewSet(APITestCase):
     def test_create_recordattachment(self):
         '''RecordAttachment should be created by POST request.'''
         url = reverse('recordattachment-list')
-
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
-        attachment_type = RecordAttachment.ATTACHMENT_TYPE_CONTENT
-        path = ''
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
+        attachment_type = 0
+        path = io.BytesIO(b'some content')
         data = {'record': record.id, 'attachment_type': attachment_type,
                 'path': path}
-
-        response = self.client.post(url, data, format='json')
-
+        response = self.client.post(url, data, format='multipart')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(RecordAttachment.objects.count(), 1)
         self.assertEqual(RecordAttachment.objects.get().record.id, record.id)
         self.assertEqual(RecordAttachment.objects.get().attachment_type,
                          attachment_type)
-        self.assertEqual(RecordAttachment.objects.get().path, path)
 
     def test_list_recordattachment(self):
         '''RecordAttachment list should be accessed by GET request.'''
@@ -194,8 +201,10 @@ class TestRecordAttachmentViewSet(APITestCase):
     def test_delete_recordattachment(self):
         '''RecordAttachment should be deleted by DELETE request.'''
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
-        recordattachment = mommy.make(training_record.models.RecordAttachment, record=record)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
+        recordattachment = mommy.make(training_record.models.RecordAttachment,
+                                      record=record)
         url = reverse('recordattachment-detail', args=(recordattachment.pk,))
 
         response = self.client.delete(url)
@@ -206,8 +215,10 @@ class TestRecordAttachmentViewSet(APITestCase):
     def test_get_recordattachment(self):
         '''RecordAttachment should be accessed by GET request.'''
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
-        recordattachment = mommy.make(training_record.models.RecordAttachment, record=record)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
+        recordattachment = mommy.make(training_record.models.RecordAttachment,
+                                      record=record)
         url = reverse('recordattachment-detail', args=(recordattachment.pk,))
         expected_keys = {'id', 'create_time', 'update_time', 'record',
                          'attachment_type', 'path'}
@@ -222,7 +233,8 @@ class TestRecordAttachmentViewSet(APITestCase):
         attachment_type0 = RecordAttachment.ATTACHMENT_TYPE_CONTENT
         attachment_type1 = RecordAttachment.ATTACHMENT_TYPE_SUMMARY
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
         recordattachment = mommy.make(training_record.models.RecordAttachment,
                                       record=record,
                                       attachment_type=attachment_type0)
@@ -243,10 +255,11 @@ class TestStatusChangeLogViewSet(APITestCase):
         url = reverse('statuschangelog-list')
 
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
         user = mommy.make(User)
-        data = {'user': user.id,
-                'record': record.id}
+        time = now()
+        data = {'user': user.id, 'time': time, 'record': record.id}
 
         response = self.client.post(url, data, format='json')
 
@@ -254,6 +267,7 @@ class TestStatusChangeLogViewSet(APITestCase):
         self.assertEqual(StatusChangeLog.objects.count(), 1)
         self.assertEqual(StatusChangeLog.objects.get().user.id, user.id)
         self.assertEqual(StatusChangeLog.objects.get().record.id, record.id)
+        self.assertEqual(StatusChangeLog.objects.get().time, time)
 
     def test_list_statuschangelog(self):
         '''StatusChangeLog list should be accessed by GET request.'''
@@ -266,8 +280,10 @@ class TestStatusChangeLogViewSet(APITestCase):
     def test_delete_statuschangelog(self):
         '''StatusChangeLog should be deleted by DELETE request.'''
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
-        statuschangelog = mommy.make(training_record.models.StatusChangeLog, record=record)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
+        statuschangelog = mommy.make(training_record.models.StatusChangeLog,
+                                     record=record)
         url = reverse('statuschangelog-detail', args=(statuschangelog.pk,))
 
         response = self.client.delete(url)
@@ -278,10 +294,13 @@ class TestStatusChangeLogViewSet(APITestCase):
     def test_get_statuschangelog(self):
         '''StatusChangeLog should be accessed by GET request.'''
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
-        statuschangelog = mommy.make(training_record.models.StatusChangeLog, record=record)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
+        statuschangelog = mommy.make(training_record.models.StatusChangeLog,
+                                     record=record)
         url = reverse('statuschangelog-detail', args=(statuschangelog.pk,))
-        expected_keys = {'id', 'record', 'time', 'user', 'pre_status', 'post_status'}
+        expected_keys = {'id', 'record', 'time', 'user', 'pre_status',
+                         'post_status'}
 
         response = self.client.get(url)
 
@@ -293,7 +312,8 @@ class TestStatusChangeLogViewSet(APITestCase):
         pre_status0 = Record.STATUS_SUBMITTED
         pre_status1 = Record.STATUS_FACULTY_ADMIN_REVIEWED
         campus_event = mommy.make(training_event.models.CampusEvent)
-        record = mommy.make(training_record.models.Record, campus_event=campus_event)
+        record = mommy.make(training_record.models.Record,
+                            campus_event=campus_event)
         statuschangelog = mommy.make(training_record.models.StatusChangeLog,
                                      record=record, pre_status=pre_status0)
         url = reverse('statuschangelog-detail', args=(statuschangelog.pk,))
