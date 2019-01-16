fdd0e49297132b708e258fe1b4558dceb750ae23
Yqzzxlj <yq736677@icloud.com>
2019-01-17 05:45:51 +0800

    added verbose_name for training_record in apps.py

--- training_record/apps.py
+++ training_record/apps.py
@@ -1,5 +1,6 @@
 '''Define how our app behave under different configs.'''
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class TrainingRecordConfig(AppConfig):
