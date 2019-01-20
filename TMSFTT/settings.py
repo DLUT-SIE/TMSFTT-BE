ac2a65aeaa6ebb43912e25f9f6fd0a505dac6042
David <youchen.du@gmail.com>
2019-01-20 20:13:30 +0800

    Improve Notification API.

--- TMSFTT/settings.py
+++ TMSFTT/settings.py
@@ -67,7 +67,7 @@ REST_FRAMEWORK = {
     ),
     'UPLOADED_FILES_USE_URL': False,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 10,
 }
 
 ROOT_URLCONF = 'TMSFTT.urls'
