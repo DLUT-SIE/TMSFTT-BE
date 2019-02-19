77fbe8bd953d2e0a3d1b71d907e68f6dd967388f
Youchen Du <youchen.du@gmail.com>
2019-02-19 14:32:21 +0800

    Add endpoints for Permission instances. Close #29.

--- auth/urls.py
+++ auth/urls.py
@@ -11,6 +11,7 @@ router = routers.SimpleRouter()
 router.register(r'users', auth.views.UserViewSet)
 router.register(r'departments', auth.views.DepartmentViewSet)
 router.register(r'user-profiles', auth.views.UserProfileViewSet)
+router.register(r'permissions', auth.views.PermissionViewSet)
 router.register(r'user-permissions', auth.views.UserPermissionViewSet)
 urlpatterns = router.urls
 
