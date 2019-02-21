1270421d4774a0cc8042bd7928975459bce084f3
Youchen Du <youchen.du@gmail.com>
2019-02-21 10:00:06 +0800

    Revert "Serve staticfiles in DEBUG mode."
    This reverts commit 3fd84bac53fcf2bc8286aee8646822c7c5f37a26.

--- TMSFTT/settings.py
+++ TMSFTT/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/django-static/'
+STATIC_URL = '/static/'
 
 
 # JWT settings
