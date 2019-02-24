5b42b4ffe51fd24ad6898b8f1f2709ee6e5b9d7c
dutlzn <2436013662@qq.com>
2019-02-24 10:12:05 +0800

    Fix a cross reference bug in training_record module
    close #40

--- training_record/utils.py
+++ training_record/utils.py
@@ -1,11 +1,12 @@
 '''Utility functions provided by training_record module.'''
 import re
 
-import training_record.models as record_models
+
 
 
 def infer_attachment_type(fname):
     '''Infer attachment_type from file name.'''
+    import training_record.models as record_models
     if re.search(r'内容', fname):
         return record_models.RecordAttachment.ATTACHMENT_TYPE_CONTENT
     if re.search(r'总结', fname):
