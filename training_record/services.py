9a0b56a65dcee113a6a448f6f4a49dc2be4e933a
Yqzzxlj <yq736677@icloud.com>
2019-04-07 16:21:40 +0800

    add unit tests. close#72

--- training_record/services.py
+++ training_record/services.py
@@ -1,8 +1,7 @@
-import xlrd
-import tempfile
-import os
-
 '''Provide services of training record module.'''
+import tempfile
+import xlrd
+
 from django.db import transaction
 from django.contrib.auth import get_user_model
 
@@ -72,16 +71,17 @@ class RecordService:
         return record
 
     @staticmethod
-    def create_campus_records_from_excel(request):
+    def create_campus_records_from_excel(file):
         '''Create a training record of off-campus training event.
 
         Parameters
         ----------
-        file: excel
+        file: InMemoryFile
             This file should have full information needed to create a
             Record of campus-event.
             The first line should be the id of each campus-event.
-            For each campus-event, the id of its participant should be below its id.
+            For each campus-event, the id of its participant should be
+            below its id.
 
         Returns
         -------
@@ -90,40 +90,35 @@ class RecordService:
 
         records = set()
 
-        #get excel from request
-        file = request.FILES['file'].read()
-        tup = tempfile.mkstemp()
-        with open(tup[0],'wb') as f:
-            f.write(file)
+        try:
+            # get information
+            tup = tempfile.mkstemp()
+            with open(tup[0], 'wb') as work_book:
+                work_book.write(file)
+            # open excel and get the first sheet
+            sheet = xlrd.open_workbook(tup[1]).sheet_by_index(0)
+        except Exception:
+            raise BadRequest('无效的表格')
 
-        #open excel
-        workbook = xlrd.open_workbook(tup[1])
+        # process for each event
+        for index in range(sheet.ncols):
 
-        # get the first sheet
-        sheet = workbook.sheet_by_index(0)
-
-        #get the number of events
-        num_event = sheet.ncols
-
-        #process for each event
-        for index in range(num_event):
-
-            #get the information
+            # get the information
             ids = sheet.col_values(index)
             event_id = ids[0]
-            user_ids =ids[1:]
+            user_ids = ids[1:]
             try:
                 campus_event = CampusEvent.objects.get(pk=event_id)
             except Exception:
-                raise BadRequest('编号为'+str(event_id)+'的活动不存在')
-            
-            #generate the record for each participant
+                raise BadRequest('编号为'+str(int(event_id))+'的活动不存在')
+
+            # generate the record for each participant
             for user_id in user_ids:
 
                 try:
                     user = User.objects.get(pk=user_id)
                 except Exception:
-                    raise BadRequest('编号为'+str(user_id)+'的用户不存在')
+                    raise BadRequest('编号为'+str(int(user_id))+'的用户不存在')
 
                 record = Record.objects.create(
                     campus_event=campus_event, user=user)
@@ -131,8 +126,3 @@ class RecordService:
                 records.add(record)
 
         return len(records)
-
-
-
-            
-            
