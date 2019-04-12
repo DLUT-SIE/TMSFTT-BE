826bc4af16c4582526ce14e4e5290dba3972ef56
Youchen Du <youchen.du@gmail.com>
2019-04-12 15:45:49 +0800

    Disable form in DRF browsable API.

--- infra/utils.py
+++ infra/utils.py
@@ -1,4 +1,5 @@
 '''Provide useful utilities shared among modules.'''
+from rest_framework.renderers import BrowsableAPIRenderer
 
 
 def format_file_size(size_in_bytes):
@@ -12,3 +13,22 @@ def format_file_size(size_in_bytes):
         unit_idx += 1
         val /= 1024
     return '{:.2f} {}'.format(val, units[unit_idx])
+
+
+class BrowsableAPIRendererWithoutForms(BrowsableAPIRenderer):
+    """Renders the browsable api, but excludes the forms."""
+
+    def get_context(self, *args, **kwargs):
+        ctx = super().get_context(*args, **kwargs)
+        ctx['display_edit_forms'] = False
+        return ctx
+    
+    def show_form_for_method(self, view, method, request, obj):
+        """We never want to do this! So just return False."""
+        return False
+
+    def get_rendered_html_form(self, data, view, method, request):
+        """Why render _any_ forms at all. This method should return 
+        rendered HTML, so let's simply return an empty string.
+        """
+        return ''
