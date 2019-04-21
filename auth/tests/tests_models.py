f7f7c4a57b0341609f2162fdff022d036f88da36
Youchen Du <youchen.du@gmail.com>
2019-04-21 10:00:59 +0800

    Add db table for DLUT-ITS data pushing.

--- auth/tests/tests_models.py
+++ auth/tests/tests_models.py
@@ -1,8 +1,12 @@
 '''Unit tests for auth models.'''
+from unittest.mock import patch, MagicMock
 from django.test import TestCase
 from model_mommy import mommy
 
-from auth.models import User, Department, UserPermission, Role, GroupPermission
+from auth.models import (
+    User, Department, UserPermission, Role, GroupPermission,
+    TeacherInformation, DepartmentInformation
+)
 
 
 class TestUser(TestCase):
@@ -67,3 +71,82 @@ class TestGroupPermission(TestCase):
             permission_id=permission_id)
 
         self.assertEqual(str(group_permission), expected_str)
+
+
+class TestTeacherInformation(TestCase):
+    '''Unit tests for TeacherInformation.'''
+    def test_get_mapping_read_file(self):
+        '''Should read from file if no cache found.'''
+        self.assertIsNone(getattr(TeacherInformation, '_gender', None))
+
+        # pylint: disable=protected-access
+        TeacherInformation._get_mapping('gender')
+
+        gender = getattr(TeacherInformation, '_gender', None)
+        self.assertIsNotNone(gender)
+        self.assertIsInstance(gender, dict)
+        # pylint: disable=unsubscriptable-object
+        self.assertEqual(gender['1'], '男性')
+        self.assertEqual(gender['not-exist'], '未知')
+
+    def test_get_xl_display(self):
+        '''Should return human-readable string.'''
+        info = TeacherInformation(xl='11')
+
+        self.assertEqual(info.get_xl_display(), '博士研究生毕业')
+
+    def test_get_zyjszc_display(self):
+        '''Should return human-readable string.'''
+        info = TeacherInformation(zyjszc='061')
+
+        self.assertEqual(info.get_zyjszc_display(), '研究员')
+
+    def test_get_xb_display(self):
+        '''Should return human-readable string.'''
+        info = TeacherInformation(xb='2')
+
+        self.assertEqual(info.get_xb_display(), '女性')
+   
+    def test_get_rzzt_display(self):
+        '''Should return human-readable string.'''
+        info = TeacherInformation(rzzt='11')
+
+        self.assertEqual(info.get_rzzt_display(), '在职')
+
+    def test_get_rjlx_display(self):
+        '''Should return human-readable string.'''
+        info = TeacherInformation(rjlx='21')
+
+        self.assertEqual(info.get_rjlx_display(), '院系机关')
+
+    def test_save_should_be_disallowed(self):
+        '''Should raise error if trying to save model.'''
+        info = TeacherInformation()
+
+        with self.assertRaisesMessage(Exception, '该表状态为只读'):
+            info.save()
+
+    def test_delete_should_be_disallowed(self):
+        '''Should raise error if trying to save model.'''
+        info = TeacherInformation()
+
+        with self.assertRaisesMessage(Exception, '该表状态为只读'):
+            info.delete()
+
+
+class TestDepartmentInformation(TestCase):
+    '''Unit tests for DepartmentInformation.'''
+    def test_save_should_be_disallowed(self):
+        '''Should raise error if trying to save model.'''
+        info = DepartmentInformation()
+
+        with self.assertRaisesMessage(Exception, '该表状态为只读'):
+            info.save()
+
+    def test_delete_should_be_disallowed(self):
+        '''Should raise error if trying to save model.'''
+        info = DepartmentInformation()
+
+        with self.assertRaisesMessage(Exception, '该表状态为只读'):
+            info.delete()
+
