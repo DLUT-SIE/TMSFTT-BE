13db9b3f391ed5e41163e8c5d18f45c5b6c63550
moriatyc <119889525@qq.com>
2019-04-21 18:49:12 +0800

    make migration

--- data_migration/migrations/0001_initial.py
+++ data_migration/migrations/0001_initial.py
@@ -265,7 +265,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('tmsftt_auth', '0001_initial'),
-        ('infra', '0002_auto_20190423_1810'),
+        ('infra', '0002_auto_20190421_1833'),
         ('training_program', '0001_initial'),
         ('training_event', '0001_initial'),
         ('training_record', '0001_initial'),
