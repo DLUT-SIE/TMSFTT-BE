e121fbd36dedbbc4aef89e764849593464c16cea
moriatyc <119889525@qq.com>
2019-04-23 21:36:47 +0800

    try to fix migrations bug

--- data_migration/migrations/0001_initial.py
+++ data_migration/migrations/0001_initial.py
@@ -265,7 +265,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('tmsftt_auth', '0001_initial'),
-        ('infra', '0002_auto_20190423_2040'),
+        ('infra', '0001_initial'),
         ('training_program', '0001_initial'),
         ('training_event', '0001_initial'),
         ('training_record', '0001_initial'),
