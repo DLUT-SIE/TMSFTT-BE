f3ac7eb13f8b38d832c748872b1114b7f8cf61f7
Youchen Du <youchen.du@gmail.com>
2019-04-23 14:21:53 +0800

    Fix data_migration dependency.

--- data_migration/migrations/0001_initial.py
+++ data_migration/migrations/0001_initial.py
@@ -20,7 +20,6 @@ from training_review.models import ReviewNote
 from auth.services import DepartmentService
 
 
-User = get_user_model()
 faker = Faker('zh_CN')
 faker.seed(0)
 
@@ -41,7 +40,7 @@ def _random_datetime(before=True, after=False):
     return now() + shift
 
 
-def _random_contents(record):
+def _random_contents(record, RecordContent):
     return [RecordContent.objects.create(
         record=record,
         content_type=content_type,
@@ -50,16 +49,16 @@ def _random_contents(record):
         if random() > 0.7]
 
 
-def _random_attachments(record):
+def _random_attachments(record, RecordAttachment):
     return [RecordAttachment.objects.create(
-        record=record,
+        record_id=record.id,
         attachment_type=attachment_type,
         path=faker.file_path(depth=3),
     ) for (attachment_type, _) in RecordAttachment.ATTACHMENT_TYPE_CHOICES
         if random() > 0.7]
 
 
-def _random_status_change_logs(record):
+def _random_status_change_logs(record, Record, StatusChangeLog):
     logs = []
     pre_status = record.status
     for (status, _) in Record.STATUS_CHOICES[1:]:
@@ -68,11 +67,11 @@ def _random_status_change_logs(record):
         if random() < 0.2:
             break
         logs.append(StatusChangeLog.objects.create(
-            record=record,
+            record_id=record.id,
             pre_status=pre_status,
             post_status=status,
             time=_random_datetime(),
-            user=record.user,
+            user_id=record.user_id,
         ))
         pre_status = status
     record.status = pre_status
@@ -80,12 +79,12 @@ def _random_status_change_logs(record):
     return logs
 
 
-def _random_review_note(record, admins):
+def _random_review_note(record, admins, Record, ReviewNote):
     if record.status in [Record.STATUS_SUBMITTED]:
         return []
     return [ReviewNote.objects.create(
-        record=record,
-        user=choice(admins),
+        record_id=record.id,
+        user_id=choice(admins).id,
         content=faker.text(20),
     )]
 
@@ -98,20 +97,23 @@ def populate_initial_data(apps, _):  # pylint: disable=all
     print('Populate auth')
     print('Populate Department')
     num_departments = 5
-    departments = [DepartmentService.create_department(
-        {'name': faker.company_prefix()}) for _ in range(num_departments)]
+    Department = apps.get_model('tmsftt_auth.Department')
+    departments = [Department.objects.create(
+        name=faker.company_prefix(),
+        raw_department_id=f'{idx}'
+    ) for idx in range(1, 1 + num_departments)]
 
     print('Populate User')
-    num_users = 100
+    num_users = 20
+    User = get_user_model()
     admin = User.objects.create_superuser(
         id=1,
         username='root',
         password='root',
         email='root@root.com',
-        department=departments[0],
+        department_id=departments[0].id,
+        raw_user_id=f'1',
     )
-    admin.roles.set(departments[0].roles.all())
-    admin.groups.set([role.group for role in departments[0].roles.all()])
     usernames = set([faker.profile()['username'] for _ in range(num_users)])
     while len(usernames) < num_users:
         usernames.add(faker.profile()['username'])
@@ -119,18 +121,21 @@ def populate_initial_data(apps, _):  # pylint: disable=all
     users = [User.objects.create_user(
         id=idx,
         username=usernames[idx-2],
-        department=departments[idx] if idx < num_departments else choice(departments),
+        department_id=departments[idx].id if idx < num_departments else choice(departments).id,
         age=randint(20, 60),
         first_name=faker.first_name(),
-        last_name=faker.last_name()) for idx in range(2, 2 + num_users)]
+        last_name=faker.last_name(),
+        raw_user_id=f'{idx}'
+    ) for idx in range(2, 2 + num_users)]
 
     print('Populate infra')
     print('Populate Notification')
     num_notifications = 200
+    Notification = apps.get_model('infra.Notification')
     notifications = [Notification.objects.create(
         time=_random_datetime(),
-        sender=choice(users),
-        recipient=choice(users),
+        sender_id=choice(users).id,
+        recipient_id=choice(users).id,
         content=faker.text(100),
         read_time=now() if idx % 4 != 0 else None,
     ) for idx in range(num_notifications)]
@@ -138,38 +143,43 @@ def populate_initial_data(apps, _):  # pylint: disable=all
     print('Populate training_program')
     print('Populate ProgramCategory')
     category_names = ('教学培训', '教学促进', '教育技术', '青年教师助课', '其他')
+    ProgramCategory = apps.get_model('training_program.ProgramCategory')
     program_categotires = [ProgramCategory.objects.create(
         name=category_name) for category_name in category_names]
 
     print('Populate ProgramForm')
     form_names = ('参与', '主讲', '评委')
+    ProgramForm = apps.get_model('training_program.ProgramForm')
     program_forms = [ProgramForm.objects.create(
         name=form_name) for form_name in form_names]
 
     print('Populate Program')
     num_programs = 15
+    Program = apps.get_model('training_program.Program')
     programs = [Program.objects.create(
         name=faker.text(10),
-        department=choice(departments),
-        category=choice(program_categotires),
+        department_id=choice(departments).id,
+        category_id=choice(program_categotires).id,
     ) for _ in range(num_programs)]
 
     print('Populate training_event')
     print('Populate CampusEvent')
-    num_campus_events = 500
+    num_campus_events = 30
+    CampusEvent = apps.get_model('training_event.CampusEvent')
     campus_events = [CampusEvent.objects.create(
         name=faker.text(10),
         time=_random_datetime(False, True),
         location=faker.street_address(),
         num_hours=random() * 4,
         num_participants=randint(10, 100),
-        program=choice(programs),
+        program_id=choice(programs).id,
         deadline=now(),
         description=faker.text(100),
     ) for _ in range(num_campus_events)]
 
     print('Populate OffCampusEvent')
-    num_off_campus_events = 500
+    num_off_campus_events = 50
+    OffCampusEvent = apps.get_model('training_event.OffCampusEvent')
     off_campus_events = [OffCampusEvent.objects.create(
         name=faker.text(10),
         time=_random_datetime(False, True),
@@ -189,28 +199,38 @@ def populate_initial_data(apps, _):  # pylint: disable=all
             off_campus_event=off_campus_event)
 
     print('Populate Enrollment')
-    num_enrollments = 200
+    num_enrollments = 20
     enrollments = []
+    from training_event.models import Enrollment
+    enroll_methods = Enrollment.ENROLL_METHOD_CHOICES
+    Enrollment = apps.get_model('training_event.Enrollment')
+    Enrollment.ENROLL_METHOD_CHOICES = enroll_methods
     for campus_event in campus_events:
         enrolled_users = sample(users, randint(2, num_users - 5))
         enrollments.extend([Enrollment.objects.create(
-            campus_event=campus_event,
-            user=user,
+            campus_event_id=campus_event.id,
+            user_id=user.id,
             enroll_method=choice(Enrollment.ENROLL_METHOD_CHOICES)[0],
             is_participated=faker.boolean(80),
         ) for user in enrolled_users])
 
     print('Populate training_record')
+    from training_record.models import Record
+    record_statuses = Record.STATUS_CHOICES
+    record_status_submitted = Record.STATUS_SUBMITTED
+    Record = apps.get_model('training_record.Record')
+    Record.STATUS_CHOICES = record_statuses
+    Record.STATUS_SUBMITTED = record_status_submitted
     off_campus_event_records = [Record.objects.create(
-        off_campus_event=off_campus_event,
-        user=choice(users),
+        off_campus_event_id=off_campus_event.id,
+        user_id=choice(users).id,
         event_coefficient=EventCoefficient.objects.get(
             off_campus_event=off_campus_event)
     ) for off_campus_event in off_campus_events]
 
     campus_event_records = [Record.objects.create(
-        campus_event=enrollment.campus_event,
-        user=enrollment.user,
+        campus_event_id=enrollment.campus_event_id,
+        user_id=enrollment.user_id,
         event_coefficient=EventCoefficient.objects.filter(
             campus_event=enrollment.campus_event)[0]
     ) for enrollment in enrollments]
@@ -218,25 +238,37 @@ def populate_initial_data(apps, _):  # pylint: disable=all
     records = off_campus_event_records + campus_event_records
 
     print('Populate RecordContent')
+    from training_record.models import RecordContent
+    content_types = RecordContent.CONTENT_TYPE_CHOICES
+    RecordContent = apps.get_model('training_record.RecordContent')
+    RecordContent.CONTENT_TYPE_CHOICES = content_types
     contents = []
     for record in records:
-        contents.extend(_random_contents(record))
+        contents.extend(_random_contents(record, RecordContent))
 
     print('Populate RecordAttachment')
     attachments = []
+    from training_record.models import RecordAttachment
+    attachment_types = RecordAttachment.ATTACHMENT_TYPE_CHOICES
+    RecordAttachment = apps.get_model('training_record.RecordAttachment')
+    RecordAttachment.ATTACHMENT_TYPE_CHOICES = attachment_types
     for record in records:
-        attachments.extend(_random_attachments(record))
+        attachments.extend(_random_attachments(record, RecordAttachment))
 
     print('Populate StatusChangeLog')
     status_change_logs = []
+    StatusChangeLog = apps.get_model('training_record.StatusChangeLog')
     for record in records:
-        status_change_logs.extend(_random_status_change_logs(record))
+        status_change_logs.extend(_random_status_change_logs(
+            record, Record, StatusChangeLog))
 
     print('Populate training_review')
     print('Populate ReviewNote')
     review_notes = []
+    ReviewNote = apps.get_model('training_review.ReviewNote')
     for record in records:
-        review_notes.extend(_random_review_note(record, users[:num_departments]))
+        review_notes.extend(_random_review_note(
+            record, users[:num_departments], Record, ReviewNote))
 
 
 class Migration(migrations.Migration):
@@ -244,7 +276,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('tmsftt_auth', '0001_initial'),
-        ('infra', '0002_auto_20190421_1833'),
+        ('infra', '0002_auto_20190423_1403'),
         ('training_program', '0001_initial'),
         ('training_event', '0001_initial'),
         ('training_record', '0001_initial'),
