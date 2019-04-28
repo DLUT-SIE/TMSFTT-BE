afbd1a684f1eec5e69f01dd23a645a2f5b2e83ef
a8520238 <360238076@qq.com>
2019-04-28 04:32:52 -0700

    增加项目下的类别字段

--- training_program/tests/tests_models.py
+++ training_program/tests/tests_models.py
@@ -1,27 +1,7 @@
 '''Unit tests for training_program models.'''
 from django.test import TestCase
 
-from training_program.models import ProgramCategory, ProgramForm, Program
-
-
-class TestProgramCategory(TestCase):
-    '''Unit tests for model ProgramCategory.'''
-    def test_str(self):
-        '''Should render string correctly.'''
-        name = 'name'
-        category = ProgramCategory(name=name)
-
-        self.assertEqual(str(category), name)
-
-
-class TestProgramForm(TestCase):
-    '''Unit tests for model ProgramForm.'''
-    def test_str(self):
-        '''Should render string correctly.'''
-        name = 'name'
-        form = ProgramForm(name=name)
-
-        self.assertEqual(str(form), name)
+from training_program.models import Program
 
 
 class TestProgram(TestCase):
