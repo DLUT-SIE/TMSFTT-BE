18b8700d23fc4cab884c8c2fa6e0f61ccdaa2c5d
Yqzzxlj <yq736677@icloud.com>
2019-04-29 09:35:57 +0800

    Add update function

--- training_record/tests/tests_serializers.py
+++ training_record/tests/tests_serializers.py
@@ -1,19 +1,36 @@
 '''Unit tests for training_record serializers.'''
+import io
 from unittest.mock import Mock, patch
 
 from django.test import TestCase
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import serializers
+from model_mommy import mommy
 
+
+from training_record.models import Record, RecordAttachment
 from training_record.serializers import (
     RecordCreateSerializer, CampusEventFeedbackSerializer)
+from training_event.models import OffCampusEvent
 
 
 # pylint: disable=no-self-use
+# pylint: disable=unused-variable
 class TestRecordCreateSerializer(TestCase):
     '''Unit tests for serializer of Record.'''
     def test_validate_attachments_too_much_attachments(self):
         '''Should raise ValidationError if there are too much attachments.'''
         serializer = RecordCreateSerializer()
+        off_campus_event = mommy.make(OffCampusEvent)
+        files = [
+            InMemoryUploadedFile(
+                io.BytesIO(b'some content'),
+                'path', 'name', 'content_type', 'size', 'charset')
+            for _ in range(3)]
+        serializer.instance = mommy.make(Record,
+                                         off_campus_event=off_campus_event)
+        attachments = [mommy.make(RecordAttachment, path=x,     # noqa
+                                  record=serializer.instance) for x in files]
         data = [Mock() for _ in range(20)]
 
         with self.assertRaisesMessage(
@@ -34,6 +51,13 @@ class TestRecordCreateSerializer(TestCase):
                 serializers.ValidationError,
                 '上传附件过大，请修改后再上传。(附件大小: 100 MB)'):
             serializer.validate_attachments(data)
+
+    @patch('training_record.serializers.RecordService')
+    def test_update(self, mocked_service):
+        '''Should update record'''
+        serializer = RecordCreateSerializer()
+        serializer.update(1, {})
+        mocked_service.update_off_campus_record_from_raw_data.assert_called()
 
 
 class TestCampusEventFeedbackSerializer(TestCase):
