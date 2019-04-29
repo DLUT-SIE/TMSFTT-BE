254124c2bc338201f5f80ad11ebddacb79935929
a8520238 <360238076@qq.com>
2019-04-29 04:30:49 -0700

    修改了命名 删除不必要的部分

--- training_program/views.py
+++ training_program/views.py
@@ -1,5 +1,5 @@
 '''Provide API views for training_program module.'''
-from rest_framework import viewsets, status, decorators
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 import training_program.models
 import training_program.serializers
@@ -13,14 +13,13 @@ class ProgramViewSet(viewsets.ModelViewSet):
 
 class ProgramCategoryViewSet(viewsets.ViewSet):
     '''get program categories from background.'''
-    @decorators.action(detail=False, methods=['GET'],
-                       url_path='')
-    def get(self, request):
-        '''return the program categories which are called '''
-        program_category = [
+    def list(self, request):
+        '''define how to get program categories'''
+        program_categories = [
             {
                 'val': item[0],
                 'name': item[1],
-            } for item in training_program.models.Program.CATEGORY_CHOICES
+            } for item in (
+                training_program.models.Program.PROGRAM_CATEGORY_CHOICES)
         ]
-        return Response(program_category, status=status.HTTP_200_OK)
+        return Response(program_categories, status=status.HTTP_200_OK)
