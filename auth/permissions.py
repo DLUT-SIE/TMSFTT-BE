d2dcad26b5b4355f48ef620b81be3fd189db5e3c
Youchen Du <youchen.du@gmail.com>
2019-05-01 16:29:45 +0800

    Remove re-define.

--- auth/permissions.py
+++ auth/permissions.py
@@ -120,15 +120,6 @@ class DjangoObjectPermissions(DjangoModelPermissions):
     permission checking logic with its parent class, so there is no need to
     add `DjangoModelPermissions` in `permission_classes` section if this class
     presented.'''
-    perms_map = {
-        'GET': ['%(app_label)s.view_%(model_name)s'],
-        'OPTIONS': ['%(app_label)s.view_%(model_name)s'],
-        'HEAD': ['%(app_label)s.view_%(model_name)s'],
-        'POST': ['%(app_label)s.add_%(model_name)s'],
-        'PUT': ['%(app_label)s.change_%(model_name)s'],
-        'PATCH': ['%(app_label)s.change_%(model_name)s'],
-        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
-    }
 
     def get_required_object_permissions(self, method, view, model_cls):
         '''Get object permissions names.
