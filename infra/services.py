57ee19b494af42e0c2fa1d61f90d5ea174edd211
Youchen Du <youchen.du@gmail.com>
2019-05-02 15:29:23 +0800

    Notification service for custom notifications.

--- infra/services.py
+++ infra/services.py
@@ -1,11 +1,24 @@
 '''Provide services of infra module.'''
 from django.utils.timezone import now
+from django.contrib.auth import get_user_model
 
 from infra.models import Notification
 
 
+User = get_user_model()
+
+
 class NotificationService:  # pylint: disable=R0903
     '''Provide services for Notification.'''
+    @classmethod
+    def _get_notification_robot(cls):
+        attr_name = '__notification_robot'
+        robot = getattr(cls, attr_name, None)
+        if robot is None:
+            robot = User.objects.get(username='notification-robot')
+            setattr(cls, attr_name, robot)
+        return robot
+
     @staticmethod
     def mark_user_notifications_as_read(user):
         '''Mark all notifications for user as read.'''
@@ -20,3 +33,24 @@ class NotificationService:  # pylint: disable=R0903
     def delete_user_notifications(user):
         '''Delete all notifications the current user received.'''
         return Notification.objects.filter(recipient=user).delete()
+
+    @staticmethod
+    def send_system_notification(recipient, content):
+        '''Send system notification.
+
+        Parameters
+        ------------
+        recipient: User or int,
+            The user or the id of the user to receive the notification.
+        content: str
+            The content of the notification.
+        '''
+        if isinstance(recipient, int):
+            recipient = User.objects.get(id=recipient)
+        elif not isinstance(recipient, User):
+            raise ValueError('Only User instance or int id is supported')
+        Notification.objects.create(
+            sender=NotificationService._get_notification_robot(),
+            recipient=recipient,
+            content=content
+        )
--- infra/tests/tests_services.py
+++ infra/tests/tests_services.py
@@ -2,14 +2,23 @@
 from unittest.mock import patch, call, Mock
 
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from model_mommy import mommy
 
 from infra.services import NotificationService
+from infra.models import Notification
+
+
+User = get_user_model()
 
 
 class TestNotificationService(TestCase):
     '''Unit tests for NotificationService.'''
     @classmethod
     def setUpTestData(cls):
+        User.objects.get_or_create(
+            username='notification-robot', defaults={'first_name': '系统通知'}
+        )
         cls.service = NotificationService
 
     @patch('infra.services.now')
@@ -37,3 +46,40 @@ class TestNotificationService(TestCase):
 
         mocked_objects.filter.assert_called_with(recipient=user)
         mocked_objects.delete.assert_called()
+
+    def test_send_system_notification_user_instance(self):
+        '''Should send notification for user instance.'''
+        user = mommy.make(User)
+        content = 'Hello World!'
+
+        self.service.send_system_notification(user, content)
+
+        notification = Notification.objects.filter(recipient=user)
+        self.assertEqual(len(notification), 1)
+        notification = notification[0]
+        self.assertEqual(notification.recipient.id, user.id)
+        self.assertEqual(notification.content, content)
+
+    def test_send_system_notification_user_id(self):
+        '''Should send notification for user id.'''
+        user = mommy.make(User)
+        content = 'Hello World!'
+
+        self.service.send_system_notification(user.id, content)
+
+        notification = Notification.objects.filter(recipient=user)
+        self.assertEqual(len(notification), 1)
+        notification = notification[0]
+        self.assertEqual(notification.recipient.id, user.id)
+        self.assertEqual(notification.content, content)
+
+    def test_send_system_notification_raise_exception(self):
+        '''Should raise exception for not supported recipient.'''
+        user = mommy.make(User)
+        content = 'Hello World!'
+
+        with self.assertRaises(ValueError):
+            self.service.send_system_notification(user.username, content)
+
+        count = Notification.objects.filter(recipient=user).count()
+        self.assertEqual(count, 0)
