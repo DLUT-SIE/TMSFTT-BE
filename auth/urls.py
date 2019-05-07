0357042ed15c2dead773437ace5e314062cda887
Eyc <1911362549@qq.com>
2019-05-07 16:40:55 +0800

    add user group viewset, serializer etc. again

--- auth/urls.py
+++ auth/urls.py
@@ -10,6 +10,7 @@ import auth.views
 router = routers.SimpleRouter()
 router.register(r'users', auth.views.UserViewSet)
 router.register(r'groups', auth.views.GroupViewSet)
+router.register(r'user-groups', auth.views.UserGroupViewSet)
 router.register(r'departments', auth.views.DepartmentViewSet)
 router.register(r'permissions', auth.views.PermissionViewSet)
 router.register(r'user-permissions', auth.views.UserPermissionViewSet)
