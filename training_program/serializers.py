4c8cb940154f730575e2dd9541205d336070a17a
a8520238 <360238076@qq.com>
2019-05-07 02:30:20 -0700

    增加slug序列化器

--- training_program/serializers.py
+++ training_program/serializers.py
@@ -8,6 +8,7 @@ class ProgramSerializer(serializers.ModelSerializer):
     '''Indicate how to serialize Program instance.'''
     category_str = serializers.CharField(source='get_category_display',
                                          read_only=True)
+    department = serializers.SlugField(read_only=True, source='name')
 
     class Meta:
         model = Program
