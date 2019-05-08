fe9ecf3bd14e2dcfbb9a5b6e3b9efe49d50496f2
wangyang <1466738696@qq.com>
2019-05-08 15:37:00 +0800

    add key in statistic list and change dict format.

--- canvas_data_warehouse/services.py
+++ canvas_data_warehouse/services.py
@@ -26,23 +26,24 @@ class CanvasDataService:
     BY_PER_CAPITA_WORKLOAD = 4
 
     STAFF_GROUPING_CHOICES = (
-        (BY_DEPARTMENT, '按学院'),
-        (BY_STAFF_TYPE, '按人员类别'),
-        (BY_STAFF_TITLE, '按职称'),
-        (BY_HIGHEST_DEGREE, '按最高学位'),
-        (BY_AGE_DISTRIBUTION, '按年龄分布')
+        (BY_DEPARTMENT, '按学院', 'BY_DEPARTMENT'),
+        (BY_STAFF_TYPE, '按人员类别', 'BY_STAFF_TYPE'),
+        (BY_STAFF_TITLE, '按职称', 'BY_STAFF_TITLE'),
+        (BY_HIGHEST_DEGREE, '按最高学位', 'BY_HIGHEST_DEGREE'),
+        (BY_AGE_DISTRIBUTION, '按年龄分布', 'BY_AGE_DISTRIBUTION')
     )
     TRAINEE_GROUPING_CHOICES = (
-        (BY_DEPARTMENT, '按学院'),
-        (BY_STAFF_TITLE, '按职称'),
-        (BY_AGE_DISTRIBUTION, '按年龄分布')
+        (BY_DEPARTMENT, '按学院', 'BY_DEPARTMENT'),
+        (BY_STAFF_TITLE, '按职称', 'BY_STAFF_TITLE'),
+        (BY_AGE_DISTRIBUTION, '按年龄分布', 'BY_AGE_DISTRIBUTION')
     )
     TRAINING_HOURS_GROUPING_CHOICES = (
-        (BY_TOTAL_STAFF_NUM, '按总人数'),
-        (BY_TOTAL_TRAINING_HOURS, '按总培训学时'),
-        (BY_PER_CAPITA_TRAINING_HOURS, '按人均培训学时'),
-        (BY_TOTAL_WORKLOAD, '按总工作量'),
-        (BY_PER_CAPITA_WORKLOAD, '按人均工作量')
+        (BY_TOTAL_STAFF_NUM, '按总人数', 'BY_TOTAL_STAFF_NUM'),
+        (BY_TOTAL_TRAINING_HOURS, '按总培训学时', 'BY_TOTAL_TRAINING_HOURS'),
+        (BY_PER_CAPITA_TRAINING_HOURS, '按人均培训学时',
+            'BY_PER_CAPITA_TRAINING_HOURS'),
+        (BY_TOTAL_WORKLOAD, '按总工作量', 'BY_TOTAL_WORKLOAD'),
+        (BY_PER_CAPITA_WORKLOAD, '按人均工作量', 'BY_PER_CAPITA_WORKLOAD')
     )
 
     @classmethod
@@ -52,24 +53,30 @@ class CanvasDataService:
     @staticmethod
     def tuple_to_dict_list(data):
         '''return a data dict list'''
-        return [{'type': key, 'name': val} for key, val in data]
+        return [{'type': type_num, 'name': name, 'key': key_name} for type_num, name, key_name in data]
 
     @classmethod
     def get_canvas_options(cls):
         '''return a data graph select dictionary'''
         statistics_type = [
             {'type': cls.STAFF_STATISTICS,
-             'option': {'name': '教职工人数统计', 'subOption': cls.tuple_to_dict_list(
-                 cls.STAFF_GROUPING_CHOICES)}},
+             'name': '教职工人数统计',
+             'key': 'STAFF_STATISTICS',
+             'subOption': cls.tuple_to_dict_list(cls.STAFF_GROUPING_CHOICES)},
             {'type': cls.TRAINEE_STATISTICS,
-             'option': {'name': '培训人数统计', 'subOption': cls.tuple_to_dict_list(
-                 cls.TRAINEE_GROUPING_CHOICES)}},
+             'name': '培训人数统计',
+             'key': 'TRAINEE_STATISTICS',
+             'subOption': cls.tuple_to_dict_list(
+                 cls.TRAINEE_GROUPING_CHOICES)},
             {'type': cls.FULL_TIME_TEACHER_TRAINED_COVERAGE,
-             'option': {'name': '专任教师培训覆盖率统计', 'subOption':
-                        cls.tuple_to_dict_list(cls.TRAINEE_GROUPING_CHOICES)}},
+             'name': '专任教师培训覆盖率统计',
+             'key': 'FULL_TIME_TEACHER_TRAINED_COVERAGE',
+             'subOption': cls.tuple_to_dict_list(
+                 cls.TRAINEE_GROUPING_CHOICES)},
             {'type': cls.TRAINING_HOURS_WORKLOAD_STATISTICS,
-             'option': {'name': '培训学时与工作量统计', 'subOption':
-                        cls.tuple_to_dict_list(
-                            cls.TRAINING_HOURS_GROUPING_CHOICES)}}
+             'name': '培训学时与工作量统计',
+             'key': 'TRAINING_HOURS_WORKLOAD_STATISTICS',
+             'subOption': cls.tuple_to_dict_list(
+                 cls.TRAINING_HOURS_GROUPING_CHOICES)}
         ]
         return statistics_type
