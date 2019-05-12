65853587f9a6182f4e4da2821ed1551f5bd7525a
wangyang <1466738696@qq.com>
2019-05-12 12:20:33 +0800

    add two aggregate data services.

--- data_warehouse/tests/tests_views.py
+++ data_warehouse/tests/tests_views.py
@@ -22,7 +22,7 @@ class AggregateDataViewSet(APITestCase):
         self.client.force_authenticate(self.user)
 
         url = reverse('aggregate-data-data') + '?method_name=staff_'\
-            'statistics&a=2&b=2019&c=2019&d=1'
+            'statistics&group_by=0&start_year=2019&end_year=2019&region=1'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         url = reverse('aggregate-data-data') + '?graph=1&a=2&'\
