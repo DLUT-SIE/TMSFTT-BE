412c82b9a3ce234ab941af7fb4c7e63340ab7c9e
dutlzn <2436013662@qq.com>
2019-05-14 16:47:36 +0800

    活动报名添加用户判断

--- training_event/tests/tests_services.py
+++ training_event/tests/tests_services.py
@@ -1,11 +1,10 @@
 '''Unit tests for training_event services.'''
 import xlrd
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils.timezone import now
 from model_mommy import mommy
-
+from django.http import HttpRequest
 from infra.exceptions import BadRequest
 from training_event.models import (
     CampusEvent, Enrollment, EventCoefficient, OffCampusEvent)
@@ -23,19 +22,34 @@ class TestEnrollmentService(TestCase):
         self.event = mommy.make(CampusEvent, num_participants=0)
         self.user = mommy.make(User)
         self.data = {'campus_event': self.event, 'user': self.user}
+        self.data_no_user = {'campus_event': self.event}
+        self.request = HttpRequest()
+        self.request.user = self.user
+        self.context = {'request': self.request, 'data': ''}
 
     def test_create_enrollment_no_more_head_counts(self):
         '''Should raise BadRequest if no more head counts for CampusEvent.'''
         with self.assertRaisesMessage(
                 BadRequest, '报名人数已满'):
-            EnrollmentService.create_enrollment(self.data)
+            EnrollmentService.create_enrollment(self.data, self.context)
 
-    def test_create_enrollment(self):
+    def test_create_enrollment_user_in_data(self):
         '''Should create enrollment.'''
         self.event.num_participants = 10
         self.event.save()
 
-        EnrollmentService.create_enrollment(self.data)
+        EnrollmentService.create_enrollment(self.data, self.context)
+
+        count = Enrollment.objects.filter(user=self.user).count()
+
+        self.assertEqual(count, 1)
+
+    def test_create_enrollment_data_without_user(self):
+        '''Should create enrollment.'''
+        self.event.num_participants = 10
+        self.event.save()
+
+        EnrollmentService.create_enrollment(self.data_no_user, self.context)
 
         count = Enrollment.objects.filter(user=self.user).count()
 
