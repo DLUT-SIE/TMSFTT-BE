50d8950d0cb87a19f9f2054a1192bfe53a7e13fb
moriatyc <119889525@qq.com>
2019-05-14 18:38:10 +0800

    debug：字段类型为char

--- auth/tasks.py
+++ auth/tasks.py
@@ -15,7 +15,7 @@ def _update_from_department_information():
     '''Scan table DepartmentInformation and update related tables.'''
     prod_logger.info('开始扫描并更新部门信息')
 
-    dlut_id = 10141
+    dlut_id = '10141'
     dlut_name = '大连理工大学'
     dlut, _ = Department.objects.get_or_create(raw_department_id=dlut_id)
     if dlut.name is None or dlut.name != dlut_name:
