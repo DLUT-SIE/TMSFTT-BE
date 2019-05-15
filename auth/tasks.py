0dbec2d1451f74ce5c6393c15d23f9b138461715
moriatyc <119889525@qq.com>
2019-05-15 17:20:40 +0800

    为定时任务中更新User信息的administrative

--- auth/tasks.py
+++ auth/tasks.py
@@ -24,6 +24,7 @@ def _update_from_department_information():
 
     raw_departments = DepartmentInformation.objects.all()
     dwid_to_department = {}
+    department_to_administrative = {}
     for raw_department in raw_departments:
         department, _ = Department.objects.get_or_create(
             raw_department_id=raw_department.dwid,
@@ -44,20 +45,43 @@ def _update_from_department_information():
             department.super_department = super_department
             updated = True
 
+        # 同步单位类型
+        if department.department_type != raw_department.dwlx:
+            department.department_type = raw_department.dwlx
+            updated = True
+
         # 同步单位名称
         if department.name != raw_department.dwmc:
             department.name = raw_department.dwmc
             updated = True
         if updated:
             department.save()
+
         dwid_to_department[raw_department.dwid] = department
-        # TODO(youchen): Create related groups
+
+        if department.super_department == dlut:
+            # 二级部门的administrative为本身
+            department_to_administrative[department] = department
+        else:
+            # 非二级部门的administrative暂为superdepartment
+            department_to_administrative[department] = (
+                department.super_department
+            )
+    # 并查集思想找administrative_department
+    for department in department_to_administrative:
+        administrative = department_to_administrative[department]
+        while administrative != department:
+            administrative = department_to_administrative[administrative]
+        department_to_administrative[department] = administrative
+    # TODO(youchen): Create related groups
+
     prod_logger.info('部门信息更新完毕')
 
-    return dwid_to_department
+    return dwid_to_department, department_to_administrative
 
 
-def _update_from_teacher_information(dwid_to_department):
+def _update_from_teacher_information(dwid_to_department,
+                                     department_to_administrative):
     '''Scan table TeacherInformation and update related tables.'''
     prod_logger.info('开始扫描并更新用户信息')
     raw_users = TeacherInformation.objects.all()
@@ -74,14 +98,21 @@ def _update_from_teacher_information(dwid_to_department):
             )
             prod_logger.warning(warn_msg)
         user.gender = User.GENDER_CHOICES_MAP[raw_user.get_xb_display()]
-        user.age = int(raw_user.nl) if raw_user.nl else 0
-        if raw_user.rxsj:
+        raw_age = int(raw_user.nl) if raw_user.nl else 0
+        user.age = raw_age
+        if raw_user.rxsj and user.onboard_time != raw_user.rxsj:
             user.onboard_time = make_aware(
                 parse_datetime(f'{raw_user.rxsj}T12:00:00'))
+
         user.tenure_status = raw_user.get_rzzt_display()
         user.education_background = raw_user.get_xl_display()
         user.technical_title = raw_user.get_zyjszc_display()
         user.teaching_type = raw_user.get_rjlx_display()
+        user.cell_phone_number = raw_user.sjh
+        user.email = raw_user.yxdz
+        user.administrative_department = (
+            department_to_administrative[user.department]
+        )
         user.save()
     prod_logger.info('用户信息更新完毕')
 
@@ -91,7 +122,11 @@ def _update_from_teacher_information(dwid_to_department):
 def update_teachers_and_departments_information():
     '''Scan table TBL_DW_INFO and TBL_JB_INFO, update related tables.'''
     try:
-        dwid_to_department = _update_from_department_information()
-        _update_from_teacher_information(dwid_to_department)
+        dwid_to_department, department_to_administrative = (
+            _update_from_department_information()
+        )
+
+        _update_from_teacher_information(dwid_to_department,
+                                         department_to_administrative)
     except Exception:
         prod_logger.exception('教师信息或部门信息更新失败')
