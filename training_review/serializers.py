2480926adb9c8b6803a54195d4d8565c76b0a41c
Eyc <1911362549@qq.com>
2019-05-18 11:21:07 +0800

    add object permission on ReviewNote

--- training_review/serializers.py
+++ training_review/serializers.py
@@ -2,10 +2,13 @@
 from rest_framework import serializers
 
 import training_review.models
-
+from training_review.services import ReviewNoteService
 
 class ReviewNoteSerializer(serializers.ModelSerializer):
     '''Indicate how to serialize ReviewNote instance.'''
     class Meta:
         model = training_review.models.ReviewNote
         fields = '__all__'
+
+    def create(self, validated_data):
+        return ReviewNoteService.create_review_note(validated_data)
