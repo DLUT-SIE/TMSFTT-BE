c1fe393626513a050cc4ea7e3970736392c6d71b
Youchen Du <youchen.du@gmail.com>
2019-05-24 20:46:05 +0800

    Optimize training_review APIs.

--- training_review/views.py
+++ training_review/views.py
@@ -11,8 +11,11 @@ import training_review.filters
 
 class ReviewNoteViewSet(viewsets.ModelViewSet):
     '''Create API views for ReviewNote.'''
-    queryset = training_review.models.ReviewNote.objects.all().order_by(
-        '-create_time')
+    queryset = (
+        training_review.models.ReviewNote.objects.all()
+        .select_related('user')
+        .order_by('-create_time')
+    )
     serializer_class = training_review.serializers.ReviewNoteSerializer
     filter_class = training_review.filters.ReviewNoteFilter
     filter_backends = (filters.DjangoObjectPermissionsFilter,
