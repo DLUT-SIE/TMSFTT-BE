926316feb11a75c24b77b34f58ceb0ca2dce4cd7
ghh82058 <guanhuih@outlook.com>
2019-05-28 20:51:46 +0800

    Validate whether user has the permission to add review note.

--- training_review/tests/tests_serializers.py
+++ training_review/tests/tests_serializers.py
@@ -1,1 +1,26 @@
 '''Unit tests for training_review serializers.'''
+from unittest.mock import Mock
+
+from django.test import TestCase
+from rest_framework import serializers
+
+from training_review.serializers import ReviewNoteSerializer
+
+
+class TestReviewNoteSerializer(TestCase):
+    '''Unit tests for serializer of ReviewNote.'''
+    def test_validate(self):
+        '''
+        Should raise ValidationError if
+        user doesn't have permission to change record.
+        '''
+        review_note = Mock()
+        request = Mock()
+        user = Mock()
+        request.user = user
+        context = {
+            'request': request
+        }
+        serializer = ReviewNoteSerializer(review_note, context=context)
+        with self.assertRaises(serializers.ValidationError):
+            serializer.validate({})
