2a0f1c0f3546a58b6d1c20825c58513eeb482793
a8520238 <360238076@qq.com>
2019-05-29 02:58:14 -0700

    增加valid department

--- training_program/serializers.py
+++ training_program/serializers.py
@@ -19,6 +19,17 @@ class ProgramSerializer(serializers.ModelSerializer):
         return ProgramService.update_program(instance, validated_data,
                                              self.context)
 
+    def validate_department(self, department):
+        '''Forbid illegal create of department.'''
+        if self.instance is not None:
+            if department.name != self.instance.department.name:
+                raise serializers.ValidationError('不可以修改培训项目的院系')
+        if self.instance is None:
+            if not self.context['request'].user.check_department_admin(
+                    department):
+                raise serializers.ValidationError('不能创建非本学院开设的培训项目')
+        return department
+
 
 class ReadOnlyProgramSerializer(serializers.ModelSerializer):
     '''Indicate how to serialize read_only Program instance.'''
