bcfeba91352809e01329b44041eaf79b2fd97cf4
dutlzn <2436013662@qq.com>
2019-05-30 09:50:06 +0800

    解决冲突.#245

--- data_warehouse/services/aggregate_data_service.py
+++ data_warehouse/services/aggregate_data_service.py
@@ -235,8 +235,6 @@ class AggregateDataService:
             cls.TABLE_NAME_TRAINING_FEEDBACK: 'table_training_feedback',
             cls.TABLE_NAME_WORKLOAD_CALCULATION: 'table_workload_calculation',
             cls.TABLE_NAME_TRAINING_RECORDS: 'table_training_records',
-            cls.TABLE_NAME_TEACHER: 'table_teacher_statistics'
-            cls.TABLE_NAME_ATTENDANCE_SHEET: 'attendance_sheet'
             cls.TABLE_NAME_TEACHER: 'table_teacher_statistics',
             cls.TABLE_NAME_ATTENDANCE_SHEET: 'attendance_sheet',
         }
