bc282f9472016979aaa64b5de69137c54b0e072b
Yqzzxlj <yq736677@icloud.com>
2019-07-07 16:18:54 +0800

    add function to send sms

--- infra/services.py
+++ infra/services.py
@@ -1,9 +1,18 @@
 '''Provide services of infra module.'''
+import base64
+import json
+import hashlib
+
+from django.conf import settings
 from django.utils.timezone import now
 from django.contrib.auth import get_user_model
+from zeep import Client
+from zeep.cache import InMemoryCache
+from zeep.transports import Transport
 
 from infra.models import Notification
 from infra.utils import prod_logger
+from infra.exceptions import InternalServerError
 
 
 User = get_user_model()
@@ -55,3 +64,92 @@ class NotificationService:  # pylint: disable=R0903
         )
         from auth.services import PermissionService
         PermissionService.assign_object_permissions(recipient, notification)
+
+
+class SOAPSMSService:
+    '''Provide service for sending sms.'''
+    def send_sms(self, smses):
+        '''Send sms to user.
+
+        Parameters
+        ------------
+        SMSes: list of dict,
+            {
+                'user_phone_number': String,
+                'sms_info': String,
+            }
+        '''
+        transport = Transport(cache=InMemoryCache())
+        try:
+            client = Client(
+                f'{settings.SOAP_BASE_URL}/SmsService?wsdl',
+                transport=transport
+            )
+        except Exception:
+            prod_logger.warning('获取WSDL失败，短信服务暂时不可用')
+            raise InternalServerError('短信服务暂时不可用')
+
+        # According to the protocol, we need encrypt our secret_key with
+        # SHA-1 and encode with base64
+        sha1 = hashlib.sha1()
+        sha1.update(settings.SOAP_AUTH_SECRET_KEY.encode())
+        secret_key = base64.b64encode(sha1.digest())
+
+        default_payload = {
+            # Auth-related
+            'tp_name': settings.SOAP_AUTH_TP_NAME,
+            'sys_id': settings.SOAP_AUTH_SYS_ID,
+            'module_id': 'sms',
+            'secret_key': secret_key.decode(),
+            'interface_method': 'sms',
+
+            # Business-related
+            # NOTE: recieve_person_info is the correct parameter name,
+            # I know it's a typo but it's required by the interface
+            'recieve_person_info': '',  # Required
+            # NOTE: Again, emial_title is the correct parameter name
+            'sms_info': '',  # Required
+            'send_priority': '3',  # Send now
+            'templet_id': '0',
+            'receipt_id': '0',
+            'send_sys_id': '1',
+        }
+
+        for sms in smses:
+            phone_number = sms.get('user_phone')
+            sms_info = sms.get('sms_info')
+            payload = default_payload.copy()
+            payload['recieve_person_info'] = self.format_recipients(
+                phone_number)
+            payload['sms_info'] = sms_info
+            sms_info = json.dumps(payload)
+            try:
+                resp = client.service.saveSmsInfo(sms_info)
+                resp = json.loads(resp)
+                if resp['result'] is False:
+                    raise Exception(resp['msg'])
+            except Exception as err:
+                msg = f'短信发送失败, 失败原因: {err}'
+                prod_logger.warning(msg)
+                raise InternalServerError('短信发送失败')
+            else:
+                msg = (
+                    f'短信发送成功, 收件人: {phone_number}, '
+                    f'消息ID: {resp["msg_id"]}'
+                )
+                prod_logger.info(msg)
+
+    def format_recipients(self, recipients):
+        '''Format recipients to string.
+
+        Recipient is formatted into
+        "Name|ID|DepartmentID|DepartmentName|Email",
+        fields are optional but vertical bar is required, multiple recipients
+        are separated by "^@^".
+        '''
+        res = ''
+        for recipient in recipients:
+            if res != '':
+                res += '^@^'
+            res += f'||||{recipient}'
+        return res
