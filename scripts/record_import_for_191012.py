bc1273bd504a2d7b8e93ffeab2060d2a44197524
Yqzzxlj <yq736677@icloud.com>
2019-10-12 11:24:04 +0800

    add script for import record

--- scripts/record_import_for_191012.py
+++ scripts/record_import_for_191012.py
@@ -18,60 +18,53 @@ from auth.models import User
 from auth.services import PermissionService
 from training_program.models import Program
 from training_event.models import CampusEvent, EventCoefficient
-from training_record.models import Record, CampusEventFeedback
+from training_record.models import Record
 
 def row_parser0(row):
-    return [row[2], row[4:10]]
+    return [row[1], row[2], row[10]]
 
 def read_workload_content(
         row_parser=row_parser0, start_row=2,
-        fpath='~/Desktop/TMSFTT/骨干教师教学能力提升培训班-考勤表-更新至第六场培训.xlsx'):
+        fpath='~/Desktop/TMSFTT/骨干教师教学能力提升培训班-考勤表-更新至第七场培训.xlsx'):
     '''Read xlsx file and generate records.'''
     workbook = xlrd.open_workbook(fpath)
     sheet = workbook.sheet_by_index(0)
     num_rows = sheet.nrows
     program = Program.objects.get(name='骨干教师教学能力提升培训班')
     events = CampusEvent.objects.filter(program=program)
-    users = {x.first_name: x for x in User.objects.all()}
-    coefficients = {}
+    event = events[6]
     for idx in tqdm(range(start_row, num_rows)):
         row = sheet.row_values(idx)
         try:
-            (username, attends) = row_parser(row)
+            (department_name, username, attend) = row_parser(row)
             role = EventCoefficient.ROLE_PARTICIPATOR
         except ValueError:
             print(idx, row)
             raise
 
-        user = users.get(username, None)
+        users = User.objects.filter(first_name=username)
+        user = None
+        if len(users) == 1:
+            user = users[0]
+        elif len(users) > 1:
+            for _user in users:
+                if _user.department:
+                    if(department_name in
+                       (_user.department.name,
+                        _user.department.super_department.name)):
+                        user = _user
         if user is None:
             print(f'Unknown User at row {idx}: {username}({username})')
             continue
 
         # Record
-        for i in range(5):
-            coefficient_key = (events[i].id, role)
-            if coefficient_key not in coefficients:
-                coefficients[coefficient_key] = EventCoefficient.objects.get(
-                    campus_event=events[i],
-                    role=role
-                )
-            coefficient = coefficients[coefficient_key]
-            if attends[i]:
-                record = Record.objects.create(
-                    campus_event=events[i],
-                    user=user,
-                    status=Record.STATUS_FEEDBACK_SUBMITTED,
-                    event_coefficient=coefficient,
-                )
-                PermissionService.assign_object_permissions(user, record)
-
-                #Feedback
-                CampusEventFeedback.objects.create(record=record, content='import by admin')
-
-        if attends[5]:
+        coefficient = EventCoefficient.objects.get(
+            campus_event=event,
+            role=role
+        )
+        if attend:
             record = Record.objects.create(
-                campus_event=events[5],
+                campus_event=event,
                 user=user,
                 status=Record.STATUS_FEEDBACK_REQUIRED,
                 event_coefficient=coefficient,
