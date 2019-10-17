562fbdb61a857c14a35749fc812651fcd3f22277
Yqzzxlj <yq736677@icloud.com>
2019-10-18 03:05:32 +0800

    add try-catch to fin bug

--- data_warehouse/services/table_export_service.py
+++ data_warehouse/services/table_export_service.py
@@ -490,8 +490,14 @@ class TableExportService:
             ptr_r += 1
         # 写入数据
         TableExportService.__write_timestamp(worksheet, ptr_r, 0)
-        _, file_path = tempfile.mkstemp()
-        workbook.save(file_path)
+        try:
+            _, file_path = tempfile.mkstemp()
+        except Exception:
+            raise BadRequest('Error here!')
+        try:
+            workbook.save(file_path)
+        except Exception:
+            raise BadRequest('Error there!')
         return file_path
 
     @staticmethod
